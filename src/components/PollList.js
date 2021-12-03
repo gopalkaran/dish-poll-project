4d1d41af0bd36c0485150ca64abc197ebf0969c3
Gopal Karan <gopalkaran@krishworks.com>
2021-12-04 01:58:28 +0530

    added polllist

--- src/components/PollList.js
+++ src/components/PollList.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
+import styles from '../css/Polllist.module.css'
 
 const PollList = () => {
 
@@ -16,17 +18,47 @@ const PollList = () => {
         console.log(dishes)
         dishes.sort((a,b)=> (a.voteCount < b.voteCount ? 1 : -1))
         console.log(dishes)
+        const ar2 = dishes.slice(1, dishes.length + 1);
+        const ar3 = dishes.slice(2, dishes.length + 1);
+        console.log(ar2)
+        console.log(ar3)
+        const maxElementFirst = dishes.reduce(function (p, v) {
+            return ( p.voteCount > v.voteCount ? p : v )
+        })
+        console.log(maxElementFirst)  
+        
+        const maxElementSecond = ar2.reduce(function (p, v) {
+            return ( p.voteCount > v.voteCount ? p : v )
+        })
+        console.log(maxElementSecond)  
 
+        const maxElementThird = ar3.reduce(function (p, v) {
+            return ( p.voteCount > v.voteCount ? p : v )
+        })
+        console.log(maxElementThird)  
 
-        // dishes.map(dish => {
-            
-        // })
+        const modifiedDishes = dishes.map(dish => {
+            if(dish.voteCount === maxElementFirst.voteCount) return {...dish, pointsReceived : 30}
+            if(dish.voteCount === maxElementSecond.voteCount) return {...dish, pointsReceived : 20}
+            if(dish.voteCount === maxElementSecond.voteCount) return {...dish, pointsReceived : 10}
+            else return dish
+
+        })
+        console.log(modifiedDishes)
+        setDishList(modifiedDishes)
     }
 
 
     return (
-        <div>
-            gopal
+        <div className={styles.listView}>
+            {
+                dishList.map(dish => {
+                       return(<Link to={`/${dish.id}`} key={dish.id} className={styles.dish} >
+                            <img src={dish.image} alt={dish.name} className={styles.dishimg}></img>
+                            <div className={styles.dishname}>{dish.dishName}</div>
+                        </Link>)
+                })
+            }
         </div>
     )
 }
